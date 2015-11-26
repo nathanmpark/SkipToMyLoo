026b7b50c7bd6c42ddc51cf893b01aa59b85429a
r.sinclairchin <r.sinclairchin@gmail.com>
2015-11-26 11:43:20 -0800

    fill in restful toilets controller

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,3 +1,3 @@
 get '/' do
-  erb :"users/index"
+  erb :"toilets/index"
 end
---
+++ app/controllers/toilets.rb
@@ -0,0 +1,53 @@
+#show all toilets
+get '/toilets' do
+  @toilets = Toilet.all
+  erb :"/toilets/index"
+end
+
+#new toilet form
+get '/toilets/new' do
+  erb :"/toilets/new"
+end
+
+#create new toilet
+post '/toilets' do
+  toilet = Toilet.new
+  if toilet.save
+    redirect '/'
+  else
+    status 400
+    flash[:errors] = toilet.errors.full_messages
+    redirect "/toilets/new"
+  end
+end
+
+#get edit page
+get '/toilets/:id/edit' do
+  @toilet = Toilet.find(params[:id])
+  erb :"/toilets/edit"
+end
+
+#show a toilet
+get '/toilets/:id' do
+  @toilet = Toilet.find(params[:id])
+  erb :"/toilets/show"
+end
+
+#submit toilet edit
+put '/toilets/:id' do
+  toilet = Toilet.find(params[:id])
+  toilet.toiletname = params[:toiletname]
+  toilet.email = params[:email]
+  if toilet.save
+    redirect '/'
+  else
+    flash[:errors] = toilet.errors.full_messages
+    redirect "/toilets/#{toilet.id}/edit"
+  end
+end
+
+#delete toilet
+delete '/toilets/:id' do
+  toilet.find(params[:id]).destroy
+  redirect "/toilets/index"
+end
