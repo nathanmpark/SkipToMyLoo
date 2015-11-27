92188dfc23d779c791b592e0a48c29c6c2eee8e4
Paul Etscheit <etscheitp@gmail.com>
2015-11-26 18:51:02 -0800

    add review partial, create JS listeners for toggling of partial and create post controller

---
+++ app/controllers/ratings.rb
@@ -0,0 +1,11 @@
+post '/ratings' do
+  p params
+ @rating = Rating.new(stars: params[:stars, review: params[:review], user_id: current_user.id, toilet_id: params[:toilet])
+  if @rating && @rating.save
+    flash[:errors] = "Review has been added!"
+    redirect "/toilets/#{params[:toilet]}"
+  else
+    flash[:errors] = "Review couldn't be added! Please try again"
+    redirect "/toilets/#{params[:toilet]}"
+  end
+end
--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,19 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
-
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  reviewListener();
+  reviewSubmitListener();
 });
+
+
+var reviewListener = function(){
+  $('#reviewButton').on('click', function(event){
+    event.preventDefault();
+    $('#rating').toggle();
+  })
+}
+
+var reviewSubmitListener = function(){
+  $('#rating').on('submit', function(event){
+    event.preventDefault();
+    $('#rating').toggle();
+  })
+}
