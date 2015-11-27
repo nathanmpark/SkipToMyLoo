e1d3f32c1fc2e3c0e4e2e5a7cfa1d038e2bf07b8
Nathan Park <nate.park90@gmail.com>
2015-11-26 22:28:31 -0800

    adjust sidebar add overflow scroll

--- app/controllers/ratings.rb
+++ app/controllers/ratings.rb
@@ -1,6 +1,8 @@
 post '/ratings' do
   p params
+
  @rating = Rating.new(stars: params[:stars], review: params[:review], user_id: current_user.id, toilet_id: params[:toilet])
+
   if @rating && @rating.save
     flash[:errors] = "Review has been added!"
     redirect "/toilets/#{params[:toilet]}"
@@ -8,4 +10,5 @@ post '/ratings' do
     flash[:errors] = "Review couldn't be added! Please try again"
     redirect "/toilets/#{params[:toilet]}"
   end
+
 end
